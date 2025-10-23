eb5d9da20938a195261f54945bca4f909fd6a506
Luiz Felipe de A. e Silva <luizfelipedn57@gmail.com>
2025-10-23 17:33:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fale-com-a-dinda',
+  webDir: 'www'
+};
+
+export default config;
